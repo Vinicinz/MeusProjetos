ba7e0df8ab968b2b7fb42dffce8013b95a7a61a7
Your Name <you@example.com>
2024-04-18 00:40:37 -0300

    att 18.04

--- UAM_Atividade/BasePorExpo.java
+++ UAM_Atividade/BasePorExpo.java
@@ -2,24 +2,37 @@ import java.util.Scanner;
 
 public class BasePorExpo {
     public static void main(String[] args) {
-        @SuppressWarnings("resource")
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        int count = 0;
-        int soma = 1;
+        int count1 = 0;
+        int count2 = 0;
+        int soma1 = 1;
+        int soma2 = 1;
+        int soma3 = 1;
+
         System.out.println("Digite o valor de base: ");
-        int base = sc.nextInt();
+        int base = scan.nextInt();
         System.out.println("digite o valor do expoente: ");
-        int expo = sc.nextInt();
+        int expo = scan.nextInt();
+
+        while (count1 < expo) {
+            soma1 *= base;
+            count1++;
+        }
+        for (int i = 0; i < expo; i++) {
+            soma2 *= base;
+        }
+        do {
+            soma3 *= base;
+            count2++;
+        } while (count2 < expo);
 
         System.out.println("Usando 'math' sua conta fica: " + Math.pow(base, expo));
+        System.out.println("Usando estrutura de repetição while sua conta fica: " + soma1);
+        System.out.println("Usando estrutura for" + soma2);
+        System.out.println(" o valor usando do while fica: " + soma3);
 
-        while (count < expo) {
-            soma *= base;
-            count++;
-        }
-        System.out.println("Usando estrutura de repetição while sua conta fica: " + soma);
-
+        scan.close();
     }
 
 }
---
+++ UAM_Atividade/DivisaoPor4e5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DivisaoPor4e5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int R4, R5, valor;
+
+        System.out.println("Digite seu valor: ");
+        valor = sc.nextInt();
+
+        R4 = valor % 4;
+        R5 = valor % 5;
+
+        if (R4 == 0 && R5 == 0) {
+            System.out.println("O valor " + valor + " e divisivel por 4 e 5");
+        } else {
+            System.out.println("O numero nao e divisivel por 4 e nem 5");
+        }
+
+        sc.close();
+
+    }
+}
