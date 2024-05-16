71b8e45559f65ff4b44d02f3ffcd94d6cd7a1c10
Your Name <you@example.com>
2024-05-16 10:18:46 -0300

    new testing commit

---
+++ UAM_Atividade/Aulas_Raul/prova.java
@@ -0,0 +1,27 @@
+
+package UAM_Atividade.Aulas_Raul;
+
+class prova {
+    public static void main(String[] args) {
+
+        int a = 19 / 5;
+        float b = 19 / 5f;
+        int c = 19 / 2;
+        float d = 19 / 2f;
+        char e = 'a';
+        char f = 98;
+        char g = 'g';
+        char h = 99;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+
+}
